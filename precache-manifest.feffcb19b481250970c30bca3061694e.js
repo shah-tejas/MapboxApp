6be4166733b64270ee51adfa616e731c25ca5239
Tejas Shah <shah.te@northeastern.edu>
2020-06-15 12:26:46 -0400

    Updates

---
+++ precache-manifest.feffcb19b481250970c30bca3061694e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2a147350a7a070daada8b5b6b220de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26c31da32f8a27c20a2e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "62e1ba2677f0f724b11a",
+    "url": "/static/js/2.79f3066e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79f3066e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c31da32f8a27c20a2e",
+    "url": "/static/js/main.f60d4fee.chunk.js"
+  },
+  {
+    "revision": "876a5e8b66b16dd64b97",
+    "url": "/static/js/runtime-main.bc8f37cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
